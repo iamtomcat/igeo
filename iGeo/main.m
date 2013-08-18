872c0ab791c7cb47f0313099f43d711eea86d532
Tom <snowmanchu@gmail.com>
2013-08-17 22:44:27 -0700

    Initial Commit

---
+++ iGeo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  iGeo
+//
+//  Created by Tom on 2013-08-17.
+//  Copyright Tom 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
